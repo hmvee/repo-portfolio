cc06c8d2da1f97dbcdc9cf0f774af62d8e33e7c1
hmvee <hannah@lecsit.com>
2026-05-13 17:14:31 -0400

    remove sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://hmvee.github.io',
   base: '/repo-portfolio',
-  integrations: [sitemap()],
   compressHTML: true,
 });
